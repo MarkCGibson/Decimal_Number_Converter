541ca8e736c28cd2319fb2384b8b77adda202215
Mark Gibson <65615605+MarkCGibson@users.noreply.github.com>
2020-05-24 06:41:57 -0500

    Add files via upload

---
+++ NumToConvert.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decimal_Converter
+{
+    class NumToConvert
+    {
+        public long Number
+        { get; set; }
+
+        public NumToConvert(long num)
+        {
+            this.Number = num;
+        }
+
+        public string ConvertToBaseX(long x, char[] values)
+        {
+            long tempNum = this.Number; 
+            int location = 0;
+            string result = "";
+            for (int i = 64; i >= 0; i--)
+            {
+                location = (int)(tempNum / Math.Pow(x, i));
+                result += values[location];
+                tempNum -= (long)Math.Pow(x, i) * location;
+            }
+            result = result.TrimStart('0');
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Decimal_Converter
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // Define arrays of possible values
+            char[] octals = { '0', '1', '2', '3', '4', '5', '6', '7' };
+            char[] hexadecimals = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+            char[] binaries = { '0', '1' };
+
+            // Print out the introduction code
+            Console.WriteLine("Welcome to the decimal number converter!");
+            
+            //define a proceed with the program variable
+            bool  proceed = true;
+            while (proceed)
+            {
+                Console.WriteLine("Please enter the number to be converted");
+
+                // User inputs the number in decimal to be converted
+                Console.Write("Number: ");
+                long number;
+
+                // Checks if the number is a decimal integer
+                while (long.TryParse(Console.ReadLine(), out number) == false)
+                {
+                    Console.WriteLine("Please make sure your number is an integer");
+                }
+
+                // Creates a num to convert object
+                NumToConvert num = new NumToConvert(number);
+
+                // Starts the output
+                Console.WriteLine("--------------------------------------------------------");
+                Console.WriteLine($"Your number in decimal is {num.Number}");
+
+                // Converts to binary
+                string resultBin = num.ConvertToBaseX(2, binaries);
+                Console.WriteLine($"Your number in binary is {resultBin}");
+
+                // Convert to Octal
+                string resultOct = num.ConvertToBaseX(8, octals);
+                Console.WriteLine($"Your number in octal is {resultOct}");
+
+                // Converts to hexadecimal
+                string resultHex = num.ConvertToBaseX(16, hexadecimals);
+                Console.WriteLine($"Your number in hexadecimal is {resultHex}\n");
+
+                // Check if the user wants to enter another number
+                Console.WriteLine("Would you like to enter another number? (Y/N)");
+                if (Console.ReadLine().ToUpper() != "Y")
+                {
+                    proceed = false;
+                }
+            }
+                 
+            Console.WriteLine("Thank you for using the decimal number converter. \nPress any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
+
